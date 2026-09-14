72556adf8529c8fdb2d821f9e56bf2c64464c6ea
Abrar-afridi <abrarkhanwc004@gmail.com>
2026-09-15 00:53:01 +0500

    Update main.go

--- go-server/main.go
+++ go-server/main.go
@@ -1,5 +1,23 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/hello"
+}
+
+func main() {
+	fileServer := http.FileServer(http.Dir("./static"))
+	http.Handle("/", fileServer)
+	http.HandleFunc("/form", formHandler)
+	http.HandleFunc("/hello", helloHandler)
+
+	fmt.Printf("Starting server at port 8080\n")
+	if error := http.ListenAndServer(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
 }
