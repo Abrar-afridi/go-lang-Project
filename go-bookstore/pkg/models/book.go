0b8daba190db7bbddee9895b6a4aec9de3860ec5
Abrar-afridi <abrarkhanwc004@gmail.com>
2026-09-23 01:01:45 +0500

    Create New func and add some code

--- go-bookstore/pkg/models/book.go
+++ go-bookstore/pkg/models/book.go
@@ -1,1 +1,44 @@
 package models
+
+import (
+	"github.com/abrar/go-bookstore/pkg/config"
+	"github.com/jinzhu/gorm"
+)
+
+var db *gorm.DB
+
+type Book struct {
+	gorm.Model
+	Name        string `json:"name"`
+	Author      string `json:"author"`
+	Publication string `json:"publication"`
+}
+
+func init() {
+	config.Connect()
+	db = config.GetDB()
+	db.AutoMigrate(&Book{})
+}
+
+func (b *Book) CreateBook() *Book {
+	db.Create(b)
+	return b
+}
+
+func GetAllBooks() []Book {
+	var books []Book
+	db.Find(&books)
+	return books
+}
+
+func GetBookById(id int64) (*Book, *gorm.DB) {
+	var getBook Book
+	result := db.Where("id = ?", id).Find(&getBook)
+	return &getBook, result
+}
+
+func DeleteBook(id int64) Book {
+	var book Book
+	db.Where("id = ?", id).Delete(&book)
+	return book
+}
--- go-movies-crud/main.go
+++ go-movies-crud/main.go
@@ -93,5 +93,4 @@ func main() {
 
 	fmt.Printf("Starting server at port 8000\n")
 	log.Fatal(http.ListenAndServe(":8000", r))
-
 }
